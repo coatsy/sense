834be51f93f3ce6a4b52f4afcebde9d5fa110aa6
Andrew Coates <coatsy@outlook.com>
2019-07-24 11:53:09 +0100

    change colours

--- animate.py
+++ animate.py
@@ -15,29 +15,30 @@ sense = SenseHat()
 
 sense.clear()
 
-white = (255,255,255)
-grey1 = (191,191,191)
-grey2 = (127,127,127)
-grey3 = (63,63,63)
-black = (0,0,0)
+colour1 = (255,127,0)
+colour2 = (191,95,0)
+colour3 = (127,63,0)
+colour4 = (63,31,0)
+colour5 = (0,0,0)
 
 for y in range(0,8):
     for x in range(0,8):
-        sense.set_pixel(x, y, white)
+        sense.set_pixel(x, y, colour1)
         sleep(0)
 
 sleep(1)
 sense.clear()
 
-for i in range(0,blocksize):
+while True:
+  for i in range(0,blocksize):
     x, y = get_pixel(i)
-    sense.set_pixel(x, y, white)
+    sense.set_pixel(x, y, colour1)
     x, y = get_pixel(i-1)
-    sense.set_pixel(x, y, grey1)
+    sense.set_pixel(x, y, colour2)
     x, y = get_pixel(i-2)
-    sense.set_pixel(x, y, grey2)
+    sense.set_pixel(x, y, colour3)
     x, y = get_pixel(i-3)
-    sense.set_pixel(x, y, grey3)
+    sense.set_pixel(x, y, colour4)
     x, y = get_pixel(i-4)
-    sense.set_pixel(x, y, black)
+    sense.set_pixel(x, y, colour5)
     sleep(0.25)
